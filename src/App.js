e4acd347bcd6dcc637f7ae409988b1f5301ae6fb
Nada Smith <ayronadas@gmail.com>
2023-11-28 20:04:48 -0500

    Creating components and importing images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <h1>Hoyas in August Event Attendees</h1>
+      <FilterSidebar filters={filters} setFilters={setFilters} />
+      <List attendees={filteredAttendees} />
+      <Footer />
     </div>
   );
 }
